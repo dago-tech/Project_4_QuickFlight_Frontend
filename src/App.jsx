4a943a00acea12a862e296fb8089aa8fa2edfdc4
danielAlex92 <daniel45675@gmail.com>
2023-12-17 22:52:49 -0500

    Fixing file names type case

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./Home/Home";
-import { FlightCreate } from "./Flights/FlightCreate";
+import { FlightCreate } from "./Flights/Flightcreate";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { UserRegistration } from "./Reservation/UserRegistration";
